05249ee7d3a1aad788b7bda942cf5fd796860534
Mika <test@Tests-MacBook-Pro.local>
2024-07-18 20:02:38 -0300

    Merge for Deployment

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require('./src/app.js')
 const { conn } = require('./src/db.js')
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 const createSeeders = require('./src/seeders.js')
 
--- src/app.js
+++ src/app.js
@@ -1,13 +1,20 @@
-const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
-const router = require("./routes");
-const app = express();
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const router = require('./routes')
+const app = express()
 
-app.use(morgan("dev"));
-app.use(express.json());
-app.use(cors());
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(cors())
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*')
+	res.header('Access-Control-Allow-Credentials', 'true')
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
+	next()
+})
 
-app.use(router);
+app.use(router)
 
-module.exports = app;
+module.exports = app
