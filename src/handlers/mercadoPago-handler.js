5e66b1a516e3532e426dda73832ef47ed20a98d1
ivanjavierbell@gmail.com <ivanjavierbell@gmail.com>
2024-07-23 18:45:27 -0300

    CancelSubscription controller handler

--- src/handlers/mercadoPago-handler.js
+++ src/handlers/mercadoPago-handler.js
@@ -1,4 +1,4 @@
-const createPreference = require('../controllers/mercadoPago-controller')
+const {createPreference, cancelSubscription} = require('../controllers/mercadoPago-controller')
 const { getUserByIdController } = require('../controllers/users-controller')
 const youArePremium = require('../mailer/newPremium')
 
@@ -42,7 +42,32 @@ const mercadoPagoNotification = async (req, res) => {
 	}
 }
 
+const cancelSubscriptionHandler = async (req, res) => {
+	try {
+	  const { userId } = req.body;
+	  console.log('Received cancel request with userId:', userId); 
+	  if (!userId) {
+		return res.status(400).json({ error: 'User ID is required' });
+	  }
+	  const user = await User.findByPk(userId);
+	  if (!user) {
+		return res.status(404).json({ error: 'User not found' });
+	  }
+	  const freePlan = await Plan.findOne({ where: { planName: 'Free' } });
+	  if (!freePlan) {
+		return res.status(404).json({ error: 'Free plan not found' });
+	  }
+	  user.planName = freePlan.planName;
+	  await user.save();
+	  return res.status(200).json({ message: 'Subscription canceled and user updated to free plan' });
+	} catch (error) {
+	  return res.status(500).json({ error: 'Internal server error' });
+	}
+  };
+  
+
 module.exports = {
 	mercadoPagoPreference,
 	mercadoPagoNotification,
+	cancelSubscriptionHandler
 }
