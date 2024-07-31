3600231537166d5d2afd4766cafce94eff2edc27
Maximiliano Altamirano <max.smp17@gmail.com>
2024-08-01 05:39:51 +1200

    get Meta data uptaded

--- src/controllers/getMetaData.js
+++ src/controllers/getMetaData.js
@@ -4,7 +4,23 @@ const { fn, col } = require('sequelize')
 const metaDateDashboard = async () => {
 	try {
 		const likeCount = await Like.count()
+
+		const projectsLikes = await Project.count({
+			include: [
+				{
+					model: Like,
+					as: 'likes',
+					attributes: [],
+					required: true,
+				},
+			],
+		})
 		const userCount = await User.count()
+		const userAdmin = await User.count({
+			where: {
+				role: 'admin',
+			},
+		})
 		const userPremium = await Plan.count({
 			where: {
 				planName: 'Premium',
@@ -31,9 +47,8 @@ const metaDateDashboard = async () => {
 			},
 		})
 
-		const incomeTotal = () => {}
-		const incomes = await User.findAll({
-			attributes: [[fn('AVG', col('plan.price')), 'averagePrice']],
+		const incomeTotal = await User.findAll({
+			attributes: [[fn('SUM', col('plan.price')), 'totalPrice']],
 			include: [
 				{
 					model: Plan,
@@ -46,26 +61,48 @@ const metaDateDashboard = async () => {
 			},
 			raw: true,
 		})
+
+		const total = incomeTotal[0].totalPrice
+		const roundedAverageTotal = total ? parseFloat(total).toFixed(2) : null
+
+		const incomes = await User.findAll({
+			attributes: [[fn('AVG', col('plan.price')), 'averagePrice']],
+
+			include: [
+				{
+					model: Plan,
+					as: 'plan',
+					attributes: [],
+				},
+			],
+			where: {
+				role: 'user',
+			},
+			raw: true,
+		})
+
 		const averagePrice = incomes[0].averagePrice
 		const roundedAveragePrice = averagePrice ? parseFloat(averagePrice).toFixed(2) : null
 
-		const linkedInPercentage = ((linkedInCount / userCount) * 100).toFixed()
+		const linkedInPercentage = ((linkedInCount / projectsCount) * 100).toFixed()
 		const githubPercentage = ((githubCount / userCount) * 100).toFixed()
 		const reviewsPercentage = ((reviewCount / projectsCount) * 100).toFixed()
-		const likesPercentage = ((likeCount / projectsCount) * 100).toFixed()
+		const likesPercentage = ((projectsLikes / projectsCount) * 100).toFixed(2)
 
 		return {
 			userCount,
+			userAdmin,
 			projectsCount,
+			roundedAverageTotal,
 			roundedAveragePrice,
 			likeCount,
+			likesPercentage,
 			githubCount,
 			linkedInCount,
 			reviewCount,
 			linkedInPercentage,
 			githubPercentage,
 			reviewsPercentage,
-			likesPercentage,
 			userPremium,
 			userFree,
 		}
