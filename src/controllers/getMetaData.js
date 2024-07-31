662a602f3a5a17506ae9b3c4516374c4e5c40d7e
Maximiliano Altamirano <max.smp17@gmail.com>
2024-08-01 04:07:14 +1200

    nre plan price

--- src/controllers/getMetaData.js
+++ src/controllers/getMetaData.js
@@ -31,6 +31,7 @@ const metaDateDashboard = async () => {
 			},
 		})
 
+		//*
 		const incomes = await User.findAll({
 			attributes: [[fn('AVG', col('plan.price')), 'averagePrice']],
 			include: [
